3dab5d4341b2678022f9bf791ee1c323c3c90610
jjl <jiajianliu10@gmail.com>
2024-09-04 22:04:38 -0700

    added init test command

---
+++ test.sh
@@ -0,0 +1,1 @@
+curl -X POST \ https://qk03j06gu7.execute-api.us-east-2.amazonaws.com/test/students \ -H 'Content-Type: application/json' \ -d '{ "student_id": "123", "name": "John Doe", "course": "Enterprise Software" }'

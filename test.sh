a42375d36b69394029070de5869f4a960da0a2f2
FoamLeave <jiajianliu10@gmail.com>
2024-09-04 22:20:41 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,3 +2,16 @@ curl -X POST \
   https://qk03j06gu7.execute-api.us-east-2.amazonaws.com/test/students \
   -H 'Content-Type: application/json' \
   -d '{ "student_id": "123", "name": "John Doe", "course": "Enterprise Software" }'
+
+
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "dynamodb:PutItem",
+      "Resource": "arn:aws:dynamodb:us-east-2:124355661443:table/StudentRecords"
+    }
+  ]
+}
+

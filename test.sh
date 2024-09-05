9dacc5103ad4ecd9266ee2c0c42356230ba768fc
jjl <jiajianliu10@gmail.com>
2024-09-04 22:06:11 -0700

    fix syntax

--- test.sh
+++ test.sh
@@ -1,1 +1,4 @@
-curl -X POST \ https://qk03j06gu7.execute-api.us-east-2.amazonaws.com/test/students \ -H 'Content-Type: application/json' \ -d '{ "student_id": "123", "name": "John Doe", "course": "Enterprise Software" }'
+curl -X POST \
+  https://qk03j06gu7.execute-api.us-east-2.amazonaws.com/test/students \
+  -H 'Content-Type: application/json' \
+  -d '{ "student_id": "123", "name": "John Doe", "course": "Enterprise Software" }'
